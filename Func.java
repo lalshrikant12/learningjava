f24306c63a3292294abdfe251f67977bcbddbca5
Shri Kant Lal <46701610+lalshrikant12@users.noreply.github.com>
2020-02-18 23:01:31 +0530

    Add files via upload

---
+++ Func.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Func {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1= new String("computer");
+		System.out.println(s1);
+		String s2=s1.toUpperCase();
+		System.out.println(s2);
+		String s3=s1.toLowerCase();
+		System.out.println(s2);
+		
+		
+		
+
+	}
+}
